455459ad15701e8200bdb26442128e9eec187ee5
Sarief <andre_pool@ukr.net>
2016-03-15 21:24:15 +0100

    simple test added
    I'm bad at testing. I understand how testing works but how to actually
    test, what values and what should I test is a little hard.

--- src/main/java/com/sarief/model/entities/Invoice.java
+++ src/main/java/com/sarief/model/entities/Invoice.java
@@ -43,4 +43,48 @@ public class Invoice {
 		this.state = state;
 	}
 	
+	/**
+	 * auto-generated hashCode
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Buyer == null) ? 0 : Buyer.hashCode());
+		result = prime * result + ((Seller == null) ? 0 : Seller.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+	/**
+	 * auto-generated equals
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Invoice other = (Invoice) obj;
+		if (Buyer == null) {
+			if (other.Buyer != null)
+				return false;
+		} else if (!Buyer.equals(other.Buyer))
+			return false;
+		if (Seller == null) {
+			if (other.Seller != null)
+				return false;
+		} else if (!Seller.equals(other.Seller))
+			return false;
+		if (id != other.id)
+			return false;
+		if (state != other.state)
+			return false;
+		return true;
+	}
+	
+	
+	
 }
---
+++ src/test/java/com/sarief/model/dao/stub/DaoInvoiceStubTest.java
@@ -0,0 +1,37 @@
+package com.sarief.model.dao.stub;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.sarief.model.entities.Invoice;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DaoInvoiceStub.class)
+@WebAppConfiguration
+public class DaoInvoiceStubTest {
+
+	@Test
+	public void test() {
+		DaoInvoiceStub dao = new DaoInvoiceStub();
+		
+		Invoice invoice = new Invoice();
+		invoice.setId(6);
+		invoice.setSeller("Sarief");
+		invoice.setBuyer("Spring");
+		
+		Invoice otherInvoice = dao.create(invoice);
+		
+		assertEquals(invoice, otherInvoice);
+		assertEquals(invoice.getId(), 3); // I've supposed that in stub 0, 1, 2 already defined 
+		
+		//assertEquals(dao.read(0), dao.read(1));
+		//assertEquals(dao.read(0), dao.read(2));
+		
+	}
+
+}
